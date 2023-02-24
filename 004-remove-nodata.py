ca23ff700fc514966317a7d9f22289efde0d1d2f
Xiaolong "Bruce" Liu <liuxiaolong125@gmail.com>
2023-02-24 14:23:01 +0800

    Update 004-remove-nodata.py

--- 004-remove-nodata.py
+++ 004-remove-nodata.py
@@ -1,34 +1,22 @@
 # -*- coding: utf-8 -*-
 '''
-* Updated on 2023/02/23
+* Updated on 2023/02/24
 * python3
 **
 * remove nodata lines
 '''
 
-from datetime import datetime, date, timedelta
+from datetime import datetime
 print('Started at', datetime.now())
 
 import pathlib
-import configparser
-
-if pathlib.Path('mytool/lots/util.py').is_file():
-	from mytool.lots.util import daterange, removeLogs
-else:
-	from lots.util import daterange, removeLogs
-
-date0 = date(2000, 1, 1)
-date1 = date.today()
-dates = daterange(date0, date1)
-
-hour0, hour1 = 0, 23
-hours = list(range(hour0, hour1+1))
-
-nodata = ['%d,%d,%d,%d,,' % (d.year, d.month, d.day, h) for d in dates for h in hours]
+import pandas as pd
 
 filenames = list(pathlib.Path('data/hourly').glob('*.csv'))
 
 for filename in filenames:
-	removeLogs(filename, nodata, headers=True)
+	data = pd.read_csv(filename, low_memory=False)
+	data2 = data.dropna(subset=['Z', 'Q'], how='all').sort_values(by=['year', 'month', 'day', 'hour'])
+	data2.to_csv(filename, index=False)
 
 print('Done at', datetime.now())
